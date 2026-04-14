c90369eadbcba5ab010ab94faafd341438c63a43
LiamOrr24 <liamorr24@gmail.com>
2026-04-13 22:08:39 -0400

    Commit#2
    Added comments

--- Image-gallery/main.js
+++ Image-gallery/main.js
@@ -1,3 +1,10 @@
+/*
+Name: Liam Orr
+File: Image-Gallery.html
+Date: 13 April 2026
+An image gallery for assignment #4
+*/
+
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
